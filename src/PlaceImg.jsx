b6e88f169819752c417b6d863358dd94b43cc18e
sumitkv31 <sumitkv31@gmail.com>
2023-05-11 18:31:37 +0530

    first commit

---
+++ src/PlaceImg.jsx
@@ -0,0 +1,15 @@
+export default function PlaceImg({ place, index = 0, className = null }) {
+  if (!place.photos?.length) {
+    return "";
+  }
+  if (!className) {
+    className = "object-cover";
+  }
+  return (
+    <img
+      className={className}
+      src={"https://bookingwebsitebackend.onrender.com/uploads/" + place.photos[index]}
+      alt=""
+    />
+  );
+}
---
+++ src/UserContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const UserContext = createContext({});
+
+export function UserContextProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [ready, setReady] = useState(false);
+  useEffect(() => {
+    if (!user) {
+      const token = localStorage.getItem("accessToken");
+
+      if (token) {
+        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+      }
+
+      axios
+        .get("/profile", { withCredentials: true })
+        .then(({ data }) => {
+          setUser(data);
+          setReady(true);
+        })
+        .catch(() => {
+          setReady(true);
+        });
+    }
+  }, [user]);
+  return (
+    <UserContext.Provider value={{ user, setUser, ready }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
