6e3cf7e3f78c608e714ed8646343ca9b3e94a58a
sumitkv31 <sumitkv31@gmail.com>
2023-06-19 20:14:22 +0530

    m

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import historyApiFallback from 'vite-plugin-history-api-fallback';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  devServer:{
-    historyApiFallback:true
-  },
-  plugins: [react()],
-
-})
+  plugins: [
+    react(),
+    historyApiFallback({
+      fallbackPath: '/',
+    }),
+  ],
+});
