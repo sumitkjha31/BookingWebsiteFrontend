9b15afd4b0f1431fe286670f32477bf434385faf
sumitkv31 <sumitkv31@gmail.com>
2023-06-19 10:00:13 +0530

    new changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -21,14 +21,14 @@ export default function Header() {
             d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
           />
         </svg>
-        <span className="font-bold text-xl">airbnb</span>
+        <span className="font-bold text-xl">ExploreHotels</span>
       </Link>
       <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
         <div>Anywhere</div>
         <div className="border-l border-gray-300"></div>
-        <div>Any week</div>
+        <div>AnyTime</div>
         <div className="border-l border-gray-300"></div>
-        <div>Add guests</div>
+        <div>AddGuests</div>
         <button className="bg-primary text-white p-1 rounded-full">
           <svg
             xmlns="http://www.w3.org/2000/svg"
